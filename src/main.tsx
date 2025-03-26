f9397eeba9781fd94f86c9691b92e795d4d71021
kesvesna <kesvesna@rambler.ru>
2025-03-26 09:38:02 +0300

    Add navbar

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import './app.css';
 
 
